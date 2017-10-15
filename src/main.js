204b84ad2bc385c139c9ab22dde37446c1611ccc
Tony Bradley <antbradley91@gmail.com>
2017-10-15 12:21:45 -0400

    done

--- src/main.js
+++ src/main.js
@@ -1,21 +1,26 @@
-const m = require('mithril')
-const css = require('sheetify')
-css('./main.css')
+/** @flow */
+/** @jsx m */
+const m = require("mithril")
+const css = require("sheetify")
+css("./main.css")
 
 const state = {
-  message: 'Start building!'
+  message: "Start building!"
 }
 
 const app = {
-  view: function () {
-    return m('div', [
-      m('h3', state.message)
-    ])
+  view(one: String) {
+    var myobj = { one: "one" }
+    return (
+      <div>
+        {state.message} {myobj}
+      </div>
+    )
   }
 }
 
-const appNode = document.body.appendChild(document.createElement('div'))
+const appNode = document.body.appendChild(document.createElement("div"))
 
-m.route(appNode, '/', {
-  '/': app
+m.route(appNode, "/", {
+  "/": app
 })
